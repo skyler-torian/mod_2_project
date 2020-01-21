26da7e3c1c1e3e48f860afacbcf7f5c9506ed65b
github username <github email address>
2020-01-21 11:48:09 -0500

    set up models and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'bids/new'
+  get 'bids/create'
+  resources :items 
+  resources :users, except: [:delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
