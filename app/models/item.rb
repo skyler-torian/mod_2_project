11679bfafb40d8df34995380af9b90972d447089
github username <github email address>
2020-01-21 14:40:52 -0500

    setup validations for users and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
     has_many :bids
     has_many :users, through: :bids
     has_many :users, through: :user_items
+    validates :name, presence: true
+    validates :desc, presence: true
+    validates :price, presence: true, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
     has_many :bids
     has_many :items, through: :bids
     has_many :items, through: :user_items
+    validates :username, presence: true, uniqueness: {case_sensitive: false}
+    validates :name, presense: true
     
 end
