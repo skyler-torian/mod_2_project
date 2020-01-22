2f9bc4da6f3c56bf6a6651ab3dd865b488e434db
github username <github email address>
2020-01-22 15:13:22 -0500

    working login page. Logout button works at top of screen once soeone logs in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # get 'users/item/new', to: 'users#new', as: 'users_item'
   get "/login", to: "sessions#new", as: "login"
   post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
   resources :items 
   resources :bids
 
